2de851ce7b1a3844e99fa25305178ec81ef7a1ad
Trishala Kaushik <trishala08@Trishalas-Air.fios-router.home>
2019-04-08 01:03:08 -0400

    addadmin commit

--- angular-app/angular-app/src/app/addadmin/addadmin.component.ts
+++ angular-app/angular-app/src/app/addadmin/addadmin.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
 
 
 @Component({
